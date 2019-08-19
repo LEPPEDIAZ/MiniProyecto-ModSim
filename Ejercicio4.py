5d4aaf577babccfa4eb63e3fcecc593a902ee546
Ana <dia151378@uvg.edu.gt>
2019-08-18 21:47:20 -0600

    menu de opciones
    al igual que los demas se corre solo con python el nombre

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -6,10 +6,27 @@ import math
 def integrand(x):
     return np.exp(-x ** 2)
 
-#-INFINITO a INFINITO
-I = quad(integrand, float(- math.inf), float(math.inf))
-print(I)
+def menu():
+    print("************MAIN MENU**************")
+    print()
 
-# 0 a 1
-Integral = quad(integrand, 0, 1)
-print(Integral)
+    choice = input("""
+                      A: Resultado de Integral de infinito a menos infinito
+                      B: Resultado de integral de 0 a 1 
+                      C: Resultado de integral con grafica de MonteCarlo
+                      Seleccione una opcion: """)
+
+    if choice == "A" or choice =="a":
+        I = quad(integrand, float(- math.inf), float(math.inf))
+        print(I)
+    elif choice == "B" or choice =="b":
+        Integral = quad(integrand, 0, 1)
+        print(Integral)
+    elif choice == "C" or choice =="c":
+        print("Resultado de MonteCarlo")
+    else:
+        print("Debes seleccionar A, B o C")
+        print("Vuelvelo a intentar")
+        menu()
+
+menu()
