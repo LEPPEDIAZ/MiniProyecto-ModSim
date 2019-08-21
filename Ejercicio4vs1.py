7baccf5730832125c43607cc245384816b05384a
Ana <dia151378@uvg.edu.gt>
2019-08-21 16:37:56 -0600

    4 espero que bien :S

--- Ejercicio4vs1.py
+++ Ejercicio4vs1.py
@@ -1,30 +1,19 @@
 import math
-from random import random
+from random import random , uniform
 import numpy as np
 from scipy.integrate import quad
-x = 0
-function = np.exp(-x ** 2)
-def g(x):
-	#Aqui definimos la función que vamos a integrar doble
-	return np.exp(-x ** 2)
-#funcion de transformacion
-#siguiendo (b-a)g(a+(b-a)y)
-#transformacion = lambda x: (1/x - 1) * g (1 + (1/x - 1))
-#transformacion = lambda x: (math.inf + math.inf) * g (-math.inf + (math.inf + math.inf))
-transformacion = lambda x: ((1 - 0) * g (1 + (1 - 0)))
-integral= quad(transformacion,0, 1)
-integral2= quad(transformacion,math.inf, -math.inf)
-print("prueba", integral)
-print("prueba", integral[0])
-print("prueba", integral2)
-print("prueba", integral2[0])
+
+def g(y):
+	return 2 * (math.e ** ((-1 * (1/y -1)**2)/ (y ** 2 )))
 def MonteCarlo(F, iteraciones):
-    for i in range(1,iteraciones):
+    for i in range(iteraciones):
+        decision = np.random.uniform(0,1) + 1
         calcular = 0
-        calcular += F(random())/iteraciones
-    return calcular
+        calcular += F(decision) 
+        calcular = calcular / iteraciones
+    return  calcular + decision
 
 print("MonteCarlo:")
-print("100: " + str(MonteCarlo(transformacion, 100)))       
-print("10,000: " + str(MonteCarlo(transformacion, 10000)))    
-print("1,000,000: " + str(MonteCarlo(transformacion, 1000000)))   
+print("100: " + str(MonteCarlo(g, 100)))       
+print("10,000: " + str(MonteCarlo(g, 10000)))    
+print("1,000,000: " + str(MonteCarlo(g, 1000000)))   
