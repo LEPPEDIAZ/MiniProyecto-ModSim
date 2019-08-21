55010a92abf02083581a3d67912692659635d119
Ana <dia151378@uvg.edu.gt>
2019-08-21 03:10:46 -0600

    almost but not there yet

--- Ejercicio4vs1.py
+++ Ejercicio4vs1.py
@@ -1,10 +1,23 @@
 import math
 from random import random
 import numpy as np
+from scipy.integrate import quad
 x = 0
 function = np.exp(-x ** 2)
+def g(x):
+	#Aqui definimos la función que vamos a integrar doble
+	return np.exp(-x ** 2)
 #funcion de transformacion
-transformacion = lambda x: (1/x - 1) * function * (1 + (1/x - 1))
+#siguiendo (b-a)g(a+(b-a)y)
+#transformacion = lambda x: (1/x - 1) * g (1 + (1/x - 1))
+#transformacion = lambda x: (math.inf + math.inf) * g (-math.inf + (math.inf + math.inf))
+transformacion = lambda x: ((1 - 0) * g (1 + (1 - 0)))
+integral= quad(transformacion,0, 1)
+integral2= quad(transformacion,math.inf, -math.inf)
+print("prueba", integral)
+print("prueba", integral[0])
+print("prueba", integral2)
+print("prueba", integral2[0])
 def MonteCarlo(F, iteraciones):
     for i in range(1,iteraciones):
         calcular = 0
